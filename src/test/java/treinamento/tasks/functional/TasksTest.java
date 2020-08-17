ead426da6152d6d71721d0e94066547a3ac70db5
Beatriz Manfron <beatriz.manfron@atomicsolutions.com.br>
2020-08-17 13:34:08 -0300

    paralelismo em docker

--- src/test/java/treinamento/tasks/functional/TasksTest.java
+++ src/test/java/treinamento/tasks/functional/TasksTest.java
@@ -1,25 +1,30 @@
 package treinamento.tasks.functional;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class TasksTest 
 {
-	public WebDriver acessarAplicacao() 
+	public WebDriver acessarAplicacao() throws MalformedURLException 
 	{
-		WebDriver driver = new ChromeDriver();
+//		WebDriver driver = new ChromeDriver();
+		DesiredCapabilities cap = DesiredCapabilities.chrome();
+		WebDriver driver = new RemoteWebDriver(new URL("http://http://192.168.99.100:4444/wd/hub"), cap);
 		driver.navigate().to("http://localhost:8001/tasks/");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
 	}
 	
 	@Test
-	public void deveSalvarTarefaComSucesso() 
+	public void deveSalvarTarefaComSucesso() throws MalformedURLException 
 	{
 		WebDriver driver = acessarAplicacao();
 		try 
@@ -47,7 +52,7 @@ public class TasksTest
 	}
 	
 	@Test
-	public void naoDeveSalvarTarefaSemDescricao() 
+	public void naoDeveSalvarTarefaSemDescricao() throws MalformedURLException 
 	{
 		WebDriver driver = acessarAplicacao();
 		try 
@@ -72,7 +77,7 @@ public class TasksTest
 	}
 	
 	@Test
-	public void naoDeveSalvarTarefaSemData() 
+	public void naoDeveSalvarTarefaSemData() throws MalformedURLException 
 	{
 		WebDriver driver = acessarAplicacao();
 		try 
@@ -97,7 +102,7 @@ public class TasksTest
 	}
 	
 	@Test
-	public void naoDeveSalvarTarefaComDataPassada() 
+	public void naoDeveSalvarTarefaComDataPassada() throws MalformedURLException 
 	{
 		WebDriver driver = acessarAplicacao();
 		try 
