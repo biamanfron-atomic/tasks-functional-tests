a59013c183bea5738ea8bd7fd498b3b2316e7c82
automacaoAtomic <automacao@atomicsolutions.com.br>
2021-02-02 08:51:11 -0300

    troca de ip

--- src/test/java/treinamento/tasks/functional/TasksTest.java
+++ src/test/java/treinamento/tasks/functional/TasksTest.java
@@ -8,6 +8,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -15,10 +16,10 @@ public class TasksTest
 {
 	public WebDriver acessarAplicacao() throws MalformedURLException 
 	{
-//		WebDriver driver = new ChromeDriver();
-		DesiredCapabilities cap = DesiredCapabilities.chrome();
-		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.99.100:4444/wd/hub"), cap);
-		driver.navigate().to("http://192.168.1.114:8001/tasks/");
+		WebDriver driver = new ChromeDriver();
+	//	DesiredCapabilities cap = DesiredCapabilities.chrome();
+	//	WebDriver driver = new RemoteWebDriver(new URL("http://192.168.99.100:4444/wd/hub"), cap);
+		driver.navigate().to("http://localhost:8001/tasks/");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
 	}
@@ -138,7 +139,7 @@ public class TasksTest
 		//inserir tarefa
 		driver.findElement(By.id("addTodo")).click();
 		driver.findElement(By.id("task")).sendKeys("Teste via Selenium");
-		driver.findElement(By.id("dueDate")).sendKeys("10/10/2010");
+		driver.findElement(By.id("dueDate")).sendKeys("10/10/2030");
 		driver.findElement(By.id("saveButton")).click();
 		String message = driver.findElement(By.id("message")).getText();
 		Assert.assertEquals("Success!", message);
